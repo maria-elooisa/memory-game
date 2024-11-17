585ed89383a44c1b5a56bfa5d9e6026f577bcea8
MariaSilva <mariaelooisa1011@gmail.com>
2024-11-17 14:46:18 -0300

    Adicionando variavel de contagem

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,6 +18,7 @@ const emojis = [
 ];
 
 let openCards = [];
+let contagem = 0;
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2: -1))
 
@@ -44,6 +45,19 @@ function checkMatch(){
     if(openCards[0].innerHTML === openCards[1].innerHTML){
         openCards[0].classList.add("boxMatch");
         openCards[1].classList.add("boxMatch");
+        contagem++
+       // Seleciona o elemento HTML pelo id
+        let elemento = document.getElementById("contagemPares");
+        // Obtém o texto atual
+        let textoAtual = elemento.textContent;
+
+        // Verifica se o texto não está vazio para evitar erros
+        if (textoAtual.length > 0) {
+            // Substitui o último caractere (mantém o texto até o penúltimo e adiciona o novo caractere)
+            let novoTexto = textoAtual.slice(0, -1) + contagem; // Substitui o último caractere por "6"
+            // Atualiza o conteúdo do elemento
+            elemento.textContent = novoTexto;
+    }       
     } else{
         openCards[0].classList.remove("boxOpen");
         openCards[1].classList.remove("boxOpen");
